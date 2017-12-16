4fff1c137784f082521d5847f98dbed3fb80520d
Paul chabinga <paulchabinga@Pauls-MacBook.local>
2017-12-16 23:06:25 +0000

    made changes to the structure of the application included redux into the application

---
+++ src/Actions/types.js
@@ -0,0 +1,10 @@
+/* Actions contained in this file
+* Exported to action creators of the application
+*/
+export const GET_POPULAR_MOVIES = "GET_POPULAR_MOVIES";
+export const GET_MOVIE = "GET_MOVIE";
+export const SEARCH_MOVIES = "SEARCH_MOVIES";
+export const GET_TOP_RATED = "GET_TOP_RATED";
+export const GET_MOVIE_REVIEWS = "GET_MOVIE_REVIEWS";
+export const GET_MOVIE_TRAILER = "GET_MOVIE_TRAILER";
+export const GET_NOW_PLAYING = "GET_NOW_PLAYING";
